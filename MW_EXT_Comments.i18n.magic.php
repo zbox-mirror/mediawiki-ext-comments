6dac5e927398422d097a13c3642a037e44e70ae0
z17cx <mail@z17.cx>
2022-09-05 20:38:00 +0000

    2022-09-05 20:38:00

--- MW_EXT_Comments.i18n.magic.php
+++ MW_EXT_Comments.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @licence GNU GPL
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['en'] = [
   'comments' => [0, 'comments'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['ru'] = [
   'comments' => [0, 'comments'],
